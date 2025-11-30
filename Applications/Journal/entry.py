54bfcb358823e1c214753ada1d1197a10118382d
Christopher Newell <46467742+Newell-ChristopherBrycen@users.noreply.github.com>
2025-11-29 19:09:21 -0600

    Update and rename journal.py to entry.py

--- Applications/Journal/entry.py
+++ Applications/Journal/entry.py
@@ -7,6 +7,7 @@ import uuid
 
 # Data Container - recieves information from the Interface. Makes interface Entry available as an object to the Journal. 
 class Entry:
+	# Constructor and core data
 	def __init__(self, title, content, entry_id=None, date=None, tags=None):
 		self.title = title 
 		self.content = content
@@ -14,7 +15,18 @@ class Entry:
 		self.date = datetime.datetime.now().isoformat()
 		self.tags = None
 
-	
+	# Mutability of fields
+	def update_title(new_title)
+	def update_content(new_content)
+
+	# State Tracking for autosave
+	def mark_dirty()
+	def mark_clean()
+	def is_dirty()
+
+	# display/storage
+	def get_summary()
+	def to_dict() 
 
 # provides busines logic and state management (comparing running-config to startup-config for autosave feature)
 class Journal:
