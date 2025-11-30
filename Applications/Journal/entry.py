3d251d930272b1af5f0795f6f7adeff779e3675f
Christopher Newell <46467742+Newell-ChristopherBrycen@users.noreply.github.com>
2025-11-29 20:22:57 -0600

    Refactor Entry class for immutability and validation
    Refactor Entry class to improve immutability and state tracking. Added methods for updating title and content with validation.

--- Applications/Journal/entry.py
+++ Applications/Journal/entry.py
@@ -8,25 +8,50 @@ import uuid
 # Data Container - recieves information from the Interface. Makes interface Entry available as an object to the Journal. 
 class Entry:
 	# Constructor and core data
-	def __init__(self, title, content, entry_id=None, date=None, tags=None):
-		self.title = title 
-		self.content = content
-		self.entry_id = str(uuid.uuid4()) # guaranteed global uniqueness
-		self.date = datetime.datetime.now().isoformat()
-		self.tags = None
+from datetime import datetime
+import uuid
 
-	# Mutability of fields
-	def update_title(new_title)
-	def update_content(new_content)
+class Entry():
+    def __init__(self, title="", content="" ):
+        # Immutable fields
+        self.entry_id = str(uuid.uuid4())
+        self.date = datetime.now()
+        
+        # Mutable Fields
+        self._title = title
+        self._content = content
+        
+        # State tracking
+        self._dirty = False
+        
 
-	# State Tracking for autosave
-	def mark_dirty()
-	def mark_clean()
-	def is_dirty()
+    def update_title(self, new_title):
+        if len(new_title) > 20:
+            raise ValueError("Title is too long - max 20 characters")
+        self._title = new_title
+        self.mark_dirty()
 
-	# display/storage
-	def get_summary()
-	def to_dict() 
+    def update_content(self, new_content):
+        if len(self.new_content) > 200:
+            raise ValueError("Content is too long - max 200 characters")
+        self._content = new_content
+        self.mark_dirty()
+
+    def mark_dirty(self):
+        self._dirty = True
+
+    def is_dirty(self):
+        return self._dirty
+
+    def mark_clean(self):
+        self._dirty = False 
+
+    def get_summary(self):
+        if len(self._content) <= 50:
+            return self._content
+        else:
+            return self._content[:50] + "..."
+
 
 # provides busines logic and state management (comparing running-config to startup-config for autosave feature)
 class Journal:
